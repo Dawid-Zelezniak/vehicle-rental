fdeadad3d8acf8682d07e739dc475bda411cf5c2
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-09-13 16:16:14 +0200

    try-catch block has been added to setAllTables method

--- src/test/java/com/vehicle/rental/zelezniak/config/DatabaseSetup.java
+++ src/test/java/com/vehicle/rental/zelezniak/config/DatabaseSetup.java
@@ -1,5 +1,6 @@
 package com.vehicle.rental.zelezniak.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 @Component
+@Slf4j
 public class DatabaseSetup {
 
     private static final String DROP = "DROP ALL OBJECTS";
@@ -17,13 +19,22 @@ public class DatabaseSetup {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void setupAllTables() throws IOException {
-        String schemaSql = new String(Files.readAllBytes(Paths.get("src/test/resources/schema.sql")));
-        String dataSql = new String(Files.readAllBytes(Paths.get("src/test/resources/data.sql")));
-        executeQueries(schemaSql,dataSql);
+    public void setupAllTables() {
+        try {
+            String schemaSql = new String(Files.readAllBytes(Paths.get("src/test/resources/schema.sql")));
+            String dataSql = new String(Files.readAllBytes(Paths.get("src/test/resources/data.sql")));
+            executeQueries(schemaSql, dataSql);
+        } catch (IOException e) {
+            logInformation(e);
+            throw new RuntimeException(e);
+        }
     }
 
-    public void dropAllTables(){
+    private void logInformation(IOException e) {
+        log.error("Exception while reading sql files: {}", e.getMessage());
+    }
+
+    public void dropAllTables() {
         executeQueries(DROP);
     }
 
