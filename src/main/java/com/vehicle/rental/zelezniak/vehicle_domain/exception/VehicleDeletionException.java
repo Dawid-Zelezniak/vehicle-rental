3389fd6b6033568d197c26bbcddd7251ff906c0c
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-09-15 10:46:59 +0200

    new exception type

---
+++ src/main/java/com/vehicle/rental/zelezniak/vehicle_domain/exception/VehicleDeletionException.java
@@ -0,0 +1,8 @@
+package com.vehicle.rental.zelezniak.vehicle_domain.exception;
+
+public class VehicleDeletionException extends RuntimeException{
+
+    public VehicleDeletionException(String message) {
+        super(message);
+    }
+}
