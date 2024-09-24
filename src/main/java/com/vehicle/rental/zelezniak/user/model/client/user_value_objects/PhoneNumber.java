d13ce1aab3c8ca35a88e2141c078560c013ead7b
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-09-24 18:59:28 +0200

    new class 'PhoneNumber' has been created

---
+++ src/main/java/com/vehicle/rental/zelezniak/user/model/client/user_value_objects/PhoneNumber.java
@@ -0,0 +1,35 @@
+package com.vehicle.rental.zelezniak.user.model.client.user_value_objects;
+
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class PhoneNumber {
+
+    private static final String E164_REGEX = "^\\+(?:[0-9] ?){6,14}[0-9]$";
+    private String number;
+
+    public PhoneNumber(String number) {
+        if (!isValid(number)) {
+            throw new IllegalArgumentException("Invalid phone number format");
+        }
+        this.number = number;
+    }
+
+    private boolean isValid(String number) {
+        Pattern pattern = Pattern.compile(E164_REGEX);
+        return pattern.matcher(number).matches();
+    }
+
+    @Override
+    public String toString() {
+        return number;
+    }
+}
---
+++ src/test/java/com/vehicle/rental/zelezniak/PhoneNumberTest.java
@@ -0,0 +1,50 @@
+package com.vehicle.rental.zelezniak;
+
+import com.vehicle.rental.zelezniak.user.model.client.user_value_objects.PhoneNumber;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@TestPropertySource("/application-test.properties")
+class PhoneNumberTest {
+
+    @Test
+    void shouldTestPhoneNumberIsValid() {
+        String[] validNumbers = {
+                "+1234567890",
+                "+491234567890",
+                "+447911123456",
+                "+14155552671",
+                "+33123456789",
+                "+61234567890",
+                "+819012345678",
+                "+919876543210",
+                "+5511987654321",
+                "+27831234567"
+        };
+
+        for (String validNumber : validNumbers) {
+            assertDoesNotThrow(() -> new PhoneNumber(validNumber));
+        }
+    }
+
+    @Test
+    void shouldTestPhoneNumberIsInvalid() {
+        String[] invalidNumbers = {
+                "1234567890",
+                "+123",
+                "+1234567890123456",
+                "491234567890",
+                "+49123abc7890",
+                "+4912345678/90",
+                "+(123)4567890",
+                ""
+        };
+
+        for (String invalidNumber : invalidNumbers) {
+            assertThrows(IllegalArgumentException.class, () -> new PhoneNumber(invalidNumber));
+        }
+    }
+}
