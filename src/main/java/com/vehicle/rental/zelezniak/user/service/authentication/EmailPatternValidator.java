2fd64b69bae034c9772d8dcab36b4420d76e9e63
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-09-24 19:06:41 +0200

    logic of validation has been changed

--- src/main/java/com/vehicle/rental/zelezniak/user/service/authentication/EmailPatternValidator.java
+++ src/main/java/com/vehicle/rental/zelezniak/user/service/authentication/EmailPatternValidator.java
@@ -1,24 +1,28 @@
 package com.vehicle.rental.zelezniak.user.service.authentication;
 
 
+import java.util.regex.Pattern;
+
 public class EmailPatternValidator {
 
     private static final String EMAIL_PATTERN =
             "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 
 
-    public static void validate(String email){
-        if(doesNotMatch(email)){
+    public static void validate(String email) {
+        if (doesNotMatch(email)) {
             throwException(email);
         }
     }
 
     private static boolean doesNotMatch(String email) {
-        return email == null || !email.matches(EMAIL_PATTERN);
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        boolean matches = pattern.matcher(email).matches();
+        return !matches;
     }
 
     private static void throwException(String email) {
-    throw new IllegalArgumentException(
-            "Email " + email + " has invalid pattern.");
+        throw new IllegalArgumentException(
+                "Email " + email + " has invalid pattern.");
     }
 }
