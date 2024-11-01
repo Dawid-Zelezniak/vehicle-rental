b69febee821ed90f552443e7eece38f11cf6d9b8
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-11-01 17:59:07 +0100

    code format

--- src/main/java/com/vehicle/rental/zelezniak/reservation/model/util/ReservationCreationRequest.java
+++ src/main/java/com/vehicle/rental/zelezniak/reservation/model/util/ReservationCreationRequest.java
@@ -3,7 +3,7 @@ package com.vehicle.rental.zelezniak.reservation.model.util;
 import com.vehicle.rental.zelezniak.common_value_objects.RentDuration;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,15 +12,10 @@ import static com.vehicle.rental.zelezniak.constants.ValidationMessages.CAN_NOT_
 /**
  * Instance of this class is used to create a reservation in status NEW.
  */
-@Getter
-@Setter
-@AllArgsConstructor
-public class ReservationCreationRequest {
+public record ReservationCreationRequest(
+        @Min(value = 1, message = "Client id can not be lower than 1")
+        Long clientId,
+        @NotNull(message = "Rent duration" + CAN_NOT_BE_NULL)
+        RentDuration duration) {
 
-
-    @Min(value = 1, message = "Client id can not be lower than 1")
-    private Long clientId;
-
-    @NotNull(message = "Rent duration" + CAN_NOT_BE_NULL)
-    private RentDuration duration;
 }
