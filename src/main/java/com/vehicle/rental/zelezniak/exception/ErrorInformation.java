a74c9b49a54ec693f156bba04060bd6f557790a5
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-11-01 13:50:11 +0100

    Add handlers for ConstraintViolation and MethodArgumentNotValid exceptions

--- src/main/java/com/vehicle/rental/zelezniak/exception/ErrorInformation.java
+++ src/main/java/com/vehicle/rental/zelezniak/exception/ErrorInformation.java
@@ -1,13 +1,27 @@
 package com.vehicle.rental.zelezniak.exception;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class ErrorInformation {
 
     private String message;
     private Integer code;
+    private Set<String> fieldValidationErrors = new HashSet<>();
+
+    public ErrorInformation(String message, Integer code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    public ErrorInformation(Integer code, Set<String> errors) {
+        this.code = code;
+        this.fieldValidationErrors = errors;
+    }
 }
--- src/main/java/com/vehicle/rental/zelezniak/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vehicle/rental/zelezniak/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.vehicle.rental.zelezniak.exception;
 
 import com.vehicle.rental.zelezniak.vehicle.exception.CriteriaAccessException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.NoSuchElementException;
+import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -32,6 +37,30 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorInformation> handleException(IllegalStateException exception) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         return createResponse(status, exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorInformation> handleException(MethodArgumentNotValidException exception) {
+        LinkedHashSet<String> errors = new LinkedHashSet<>();
+
+        BindingResult bindingResult = exception.getBindingResult();
+        List<ObjectError> allErrors = bindingResult.getAllErrors();
+        allErrors.forEach(error -> {
+            String message = error.getDefaultMessage();
+            errors.add(message);
+        });
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return ResponseEntity.status(status).body(new ErrorInformation(status.value(), errors));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorInformation> handleException(ConstraintViolationException exception) {
+        LinkedHashSet<String> errors = new LinkedHashSet<>();
+
+        Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
+        constraintViolations.forEach(constraintViolation -> errors.add(constraintViolation.getMessage()));
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return ResponseEntity.status(status).body(new ErrorInformation(status.value(), errors));
     }
 
     @ExceptionHandler(Exception.class)
