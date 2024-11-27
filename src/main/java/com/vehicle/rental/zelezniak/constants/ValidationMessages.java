9ad0dab13c8b95a08686eee5f499fd74225a72c6
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-11-27 09:24:14 +0100

    messages moved from InputValidator class to ValidationMessages

--- src/main/java/com/vehicle/rental/zelezniak/constants/ValidationMessages.java
+++ src/main/java/com/vehicle/rental/zelezniak/constants/ValidationMessages.java
@@ -4,6 +4,14 @@ public class ValidationMessages {
 
     public static final String CAN_NOT_BE_BLANK = " can not be blank.";
     public static final String CAN_NOT_BE_NULL = " can not be null.";
+    public static final String VEHICLE_ID_NOT_NULL = "Vehicle id" + CAN_NOT_BE_NULL;
+    public static final String VEHICLE_NOT_NULL = "Vehicle" + CAN_NOT_BE_NULL;
+    public static final String CLIENT_ID_NOT_NULL = "Client id" + CAN_NOT_BE_NULL;
+    public static final String CLIENT_NOT_NULL = "Client" + CAN_NOT_BE_NULL;
+    public static final String CLIENT_EMAIL_NOT_NULL = "Client email" + CAN_NOT_BE_NULL;
+    public static final String RENT_ID_NOT_NULL = "Rent id" + CAN_NOT_BE_NULL;
+    public static final String RENT_NOT_NULL = "Rent" + CAN_NOT_BE_NULL;
+    public static final String RESERVATION_ID_NOT_NULL = "Reservation id" + CAN_NOT_BE_NULL;
 
     private ValidationMessages() {
 
