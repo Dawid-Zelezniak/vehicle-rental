85ef2b498217051056f92e03c0757f2c63cc67c5
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-11-27 10:17:17 +0100

    validates vehicle is still available

---
+++ src/main/java/com/vehicle/rental/zelezniak/vehicle/service/validation/AvailableVehiclesValidator.java
@@ -0,0 +1,29 @@
+package com.vehicle.rental.zelezniak.vehicle.service.validation;
+
+import com.vehicle.rental.zelezniak.common_value_objects.RentDuration;
+import com.vehicle.rental.zelezniak.vehicle.model.vehicles.Vehicle;
+import com.vehicle.rental.zelezniak.vehicle.service.AvailableVehiclesRetriever;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AvailableVehiclesValidator {
+
+    private final AvailableVehiclesRetriever vehiclesRetriever;
+
+    public void checkIfVehicleIsStillAvailable(RentDuration duration, Long vehicleId) {
+        List<Long> vehicleIds = vehiclesRetriever.findVehiclesAvailableInPeriod(duration)
+                .stream()
+                .map(Vehicle::getId)
+                .toList();
+
+        if (!vehicleIds.contains(vehicleId)) {
+            throw new IllegalArgumentException("Vehicle with id: " + vehicleId + " is already rented or reserved.");
+        }
+    }
+}
