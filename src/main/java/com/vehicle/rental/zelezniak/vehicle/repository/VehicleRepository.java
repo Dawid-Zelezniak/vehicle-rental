75a1ad8940c53158e4e941387605cf33db24cf59
Dawid Żeleźniak <zelekdidi@tlen.pl>
2024-12-16 19:54:17 +0100

    new method to find vehicle by registration

--- src/main/java/com/vehicle/rental/zelezniak/vehicle/repository/VehicleRepository.java
+++ src/main/java/com/vehicle/rental/zelezniak/vehicle/repository/VehicleRepository.java
@@ -6,6 +6,7 @@ import com.vehicle.rental.zelezniak.vehicle.model.vehicles.Vehicle;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,19 +14,11 @@ import org.springframework.data.repository.query.Param;
 import java.util.Collection;
 import java.util.Set;
 
-public interface VehicleRepository extends JpaRepository<Vehicle,Long>{
+public interface VehicleRepository extends JpaRepository<Vehicle, Long>, JpaSpecificationExecutor<Vehicle> {
 
     boolean existsByVehicleInformationRegistrationNumber(RegistrationNumber registrationNumber);
 
-    Page<Vehicle> findByVehicleInformationModel(String model, Pageable pageable);
-
-    Page<Vehicle> findByVehicleInformationBrand(String brand, Pageable pageable);
-
-    Page<Vehicle> findByVehicleInformationRegistrationNumber(RegistrationNumber registration, Pageable pageable);
-
-    Page<Vehicle> findByVehicleInformationProductionYear(Year year, Pageable pageable);
-
-    Page<Vehicle> findByStatus(@Param("status") Vehicle.Status status, Pageable pageable);
+    Vehicle findByVehicleInformationRegistrationNumber(RegistrationNumber registrationNumber);
 
     @Query("SELECT v FROM Vehicle v WHERE v.id NOT IN :idSet AND v.status = 'AVAILABLE'")
     Page<Vehicle> findVehiclesByIdNotIn(@Param("idSet") Set<Long> vehiclesIds, Pageable pageable);
@@ -34,10 +27,10 @@ public interface VehicleRepository extends JpaRepository<Vehicle,Long>{
     Collection<Vehicle> findVehiclesByIdNotIn(@Param("idSet") Set<Long> vehiclesIds);
 
     @Modifying
-    @Query(nativeQuery = true,value = "DELETE FROM reserved_vehicles rv WHERE rv.vehicle_id = :id")
+    @Query(nativeQuery = true, value = "DELETE FROM reserved_vehicles rv WHERE rv.vehicle_id = :id")
     void deleteFromReservedVehicles(Long id);
 
     @Modifying
-    @Query(nativeQuery = true,value = "DELETE FROM rented_vehicles rv WHERE rv.vehicle_id = :id")
+    @Query(nativeQuery = true, value = "DELETE FROM rented_vehicles rv WHERE rv.vehicle_id = :id")
     void deleteFromRentedVehicles(Long id);
 }
