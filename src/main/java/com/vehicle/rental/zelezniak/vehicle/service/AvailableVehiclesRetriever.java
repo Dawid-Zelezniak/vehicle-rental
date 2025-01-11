a5c976c3c157d69152e8235722b37586d6f78c06
Dawid Żeleźniak <zelekdidi@tlen.pl>
2025-01-11 11:10:01 +0100

    methods has been renamed

--- src/main/java/com/vehicle/rental/zelezniak/vehicle/service/AvailableVehiclesRetriever.java
+++ src/main/java/com/vehicle/rental/zelezniak/vehicle/service/AvailableVehiclesRetriever.java
@@ -1,15 +1,17 @@
 package com.vehicle.rental.zelezniak.vehicle.service;
 
-import com.vehicle.rental.zelezniak.reservation.repository.ReservationRepository;
-import com.vehicle.rental.zelezniak.vehicle.model.vehicles.Vehicle;
-import com.vehicle.rental.zelezniak.vehicle.repository.VehicleRepository;
 import com.vehicle.rental.zelezniak.common_value_objects.RentDuration;
 import com.vehicle.rental.zelezniak.rent.repository.RentRepository;
-import jakarta.persistence.EntityManager;
+import com.vehicle.rental.zelezniak.reservation.repository.ReservationRepository;
+import com.vehicle.rental.zelezniak.vehicle.model.dto.AvailableVehiclesCriteriaSearchRequest;
+import com.vehicle.rental.zelezniak.vehicle.model.vehicles.Vehicle;
+import com.vehicle.rental.zelezniak.vehicle.repository.VehicleRepository;
+import com.vehicle.rental.zelezniak.vehicle.service.criteria_search.VehicleSpecification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,26 +27,39 @@ import java.util.Set;
 @Slf4j
 public class AvailableVehiclesRetriever {
 
+    private final VehicleRepository vehicleRepository;
     private final RentRepository rentRepository;
     private final ReservationRepository reservationRepository;
-    private final VehicleRepository vehicleRepository;
 
-    public Page<Vehicle> findVehiclesAvailableInPeriod(RentDuration duration, Pageable pageable) {
-        Set<Long> unavailableVehiclesIdsInPeriod = findReservedAndRentedVehiclesInPeriod(duration);
-        return vehicleRepository.findVehiclesByIdNotIn(unavailableVehiclesIdsInPeriod, pageable);
+    @Transactional(readOnly = true)
+    public Page<Vehicle> findAvailableVehiclesByCriteria(AvailableVehiclesCriteriaSearchRequest searchRequest, Pageable pageable) {
+        Set<Long> unavailableVehiclesIdsInPeriod = findReservedAndRentedVehiclesIdsInPeriod(searchRequest.duration());
+        Specification<Vehicle> specification = VehicleSpecification.buildSpecificationForAvailableVehicles(
+                searchRequest, unavailableVehiclesIdsInPeriod);
+        return vehicleRepository.findAll(specification, pageable);
     }
 
-    public Collection<Vehicle> findVehiclesAvailableInPeriod(RentDuration duration) {
-        Set<Long> unavailableVehiclesIdsInPeriod = findReservedAndRentedVehiclesInPeriod(duration);
+    @Transactional(readOnly = true)
+    public Collection<Vehicle> findAvailableVehiclesByRentDuration(RentDuration duration) {
+        Set<Long> unavailableVehiclesIdsInPeriod = findReservedAndRentedVehiclesIdsInPeriod(duration);
         return vehicleRepository.findVehiclesByIdNotIn(unavailableVehiclesIdsInPeriod);
     }
 
-    private Set<Long> findReservedAndRentedVehiclesInPeriod(RentDuration duration) {
+    private Set<Long> findReservedAndRentedVehiclesIdsInPeriod(RentDuration duration) {
         LocalDateTime start = duration.getRentalStart();
         LocalDateTime end = duration.getRentalEnd();
-        Set<Long> unavailableForRents = rentRepository.findUnavailableVehicleIdsForRentInPeriod(start, end);
-        Set<Long> unavailableForReservations = reservationRepository.unavailableVehicleIdsForReservationInPeriod(start, end);
+        Set<Long> unavailableForRents = findUnavailableVehicleIdsForRents(start, end);
+        Set<Long> unavailableForReservations = findUnavailableVehicleIdsForReservations(start, end);
         unavailableForRents.addAll(unavailableForReservations);
         return unavailableForRents;
     }
+
+    private Set<Long> findUnavailableVehicleIdsForRents(LocalDateTime start, LocalDateTime end) {
+        return rentRepository.findUnavailableVehicleIdsForRentInPeriod(start, end);
+    }
+
+    private Set<Long> findUnavailableVehicleIdsForReservations(LocalDateTime start, LocalDateTime end) {
+        return reservationRepository.unavailableVehicleIdsForReservationInPeriod(start, end);
+    }
+
 }
